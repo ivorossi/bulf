6860f77a6a0ae32ed59a7d07a45f748f0c7b3f8e
ivo <rossiivomartin@gmail.com>
2024-10-23 01:29:22 -0300

    modal para crear gender

--- frontend/bulf/src/components/admin/AdminPanel.jsx
+++ frontend/bulf/src/components/admin/AdminPanel.jsx
@@ -2,29 +2,90 @@ import { useState } from 'react';
 import './AdminPanel.css';
 import Modal from 'react-modal';
 import ProductForm from './ProductForm';
+import GenderForm from './GenderForm';
+import CategoryForm from './CategoryForm';
+import SubcategoryForm from './SubcategoryForm';
 
 const AdminPanel = () => {
     const [isProductFormOpen, setIsProductFormOpen] = useState(false);
+    const [isGenderFormOpen, setIsGenderFormOpen] = useState(false);
+    const [isCategoryFormOpen, setIsCategoryFormOpen] = useState(false);
+    const [isSubcategoryFormOpen, setIsSubcategoryFormOpen] = useState(false);
+
     const toggleProductFormModal = () => {
         setIsProductFormOpen(!isProductFormOpen);
     };
 
-    const closeModal = () => {
-        isProductFormOpen(false);
+    const toggleGenderFormModal = () => {
+        setIsGenderFormOpen(!isGenderFormOpen);
     };
+
+    const toggleCategoryFormModal = () => {
+        setIsCategoryFormOpen(!isCategoryFormOpen);
+    };
+
+    const toggleSubcategoryFormModal = () => {
+        setIsSubcategoryFormOpen(!isSubcategoryFormOpen);
+    };
+
+    const closeModal = () => {
+        setIsProductFormOpen(false);
+        setIsGenderFormOpen(false);
+        setIsCategoryFormOpen(false);
+        setIsSubcategoryFormOpen(false);
+    };
+
     return (
         <div className="admin-panel">
             <h1 className="admin-title">Admin Panel</h1>
             <div className="admin-buttons">
-                <button className="admin-button" onClick={() => {/* Lógica para crear género */ }}>Crear Género</button>
-                <button className="admin-button" onClick={() => {/* Lógica para crear categoría */ }}>Crear Categoría</button>
-                <button className="admin-button" onClick={() => {/* Lógica para crear subcategoría */ }}>Crear Subcategoría</button>
+                <button className="admin-button" onClick={toggleGenderFormModal}>Crear Género</button>
+                <button className="admin-button" onClick={toggleCategoryFormModal}>Crear Categoría</button>
+                <button className="admin-button" onClick={toggleSubcategoryFormModal}>Crear Subcategoría</button>
                 <button className="admin-button" onClick={toggleProductFormModal}>Crear Producto</button>
             </div>
+
+            {/* Modal para Crear Género */}
+            <Modal
+                isOpen={isGenderFormOpen}
+                onRequestClose={toggleGenderFormModal}
+                contentLabel="Gender Modal"
+                className="modal"
+                overlayClassName="modal-overlay"
+            >
+                <button onClick={toggleGenderFormModal} className="close-modal">X</button>
+                <GenderForm closeModal={closeModal} />
+            </Modal>
+
+            {/* Modal para Crear Categoría */}
+            <Modal
+                isOpen={isCategoryFormOpen}
+                onRequestClose={toggleCategoryFormModal}
+                contentLabel="Category Modal"
+                className="modal"
+                overlayClassName="modal-overlay"
+            >
+                <button onClick={toggleCategoryFormModal} className="close-modal">X</button>
+                <CategoryForm closeModal={closeModal} />
+            </Modal>
+
+            {/* Modal para Crear Subcategoría */}
+            <Modal
+                isOpen={isSubcategoryFormOpen}
+                onRequestClose={toggleSubcategoryFormModal}
+                contentLabel="Subcategory Modal"
+                className="modal"
+                overlayClassName="modal-overlay"
+            >
+                <button onClick={toggleSubcategoryFormModal} className="close-modal">X</button>
+                <SubcategoryForm closeModal={closeModal} />
+            </Modal>
+
+            {/* Modal para Crear Producto */}
             <Modal
                 isOpen={isProductFormOpen}
                 onRequestClose={toggleProductFormModal}
-                contentLabel="Login Modal"
+                contentLabel="Product Modal"
                 className="modal"
                 overlayClassName="modal-overlay"
             >
@@ -33,6 +94,6 @@ const AdminPanel = () => {
             </Modal>
         </div>
     );
-}
+};
 
 export default AdminPanel;
