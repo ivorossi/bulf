965171bbb93a97bbe69d39353cadc2813675f1df
ivo <rossiivomartin@gmail.com>
2024-10-23 20:07:35 -0300

    fix product service

--- backend/bulf/src/main/java/com/recode/bulf/service/ProductService.java
+++ backend/bulf/src/main/java/com/recode/bulf/service/ProductService.java
@@ -55,7 +55,6 @@ public class ProductService {
     public Page<Product> getPagedProducts(int page, int size) {
         return productRepository.findAllOrderedByIdDesc(createPageRequest(page, size, "id"));
     }
-
     public boolean deleteProductById(Long id) {
         if (productRepository.existsById(id)) {
             productRepository.deleteById(id);
@@ -65,7 +64,6 @@ public class ProductService {
         }
     }
 
-    @Autowired
     public Product updateProduct(Long id, Product updatedProduct) {
         Optional<Product> existingProductOpt = productRepository.findById(id);
         if (existingProductOpt.isPresent()) {
